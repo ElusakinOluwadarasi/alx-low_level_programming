bf8fd21744a93586fe1c5699a2baf1f041e481ed
ElusakinOluwadarasi <elusanks@gmail.com>
2022-09-13 15:11:44 -0700

    update

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+i#include "main.h"
+
+/**
+ * print_alphabet - this function prints tha alphabet a-z
+ */
+void print_alphabet(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		_putchar(alpha);
+	}
+
+		_putchar('\n');
+}
